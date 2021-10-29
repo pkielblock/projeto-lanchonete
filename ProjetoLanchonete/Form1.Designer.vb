748aa107f9666f23b8209f62ad5fc284cb82307f
Pedro Kielblock <p.kielblock123@gmail.com>
2021-10-29 18:30:02 -0300

    Login

--- ProjetoLanchonete/Form1.Designer.vb
+++ ProjetoLanchonete/Form1.Designer.vb
@@ -22,30 +22,90 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblLogin = New System.Windows.Forms.Label()
+        Me.lblSenha = New System.Windows.Forms.Label()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnFechar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblLogin
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(349, 187)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 15)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Testing"
+        Me.lblLogin.AutoSize = True
+        Me.lblLogin.Location = New System.Drawing.Point(12, 19)
+        Me.lblLogin.Name = "lblLogin"
+        Me.lblLogin.Size = New System.Drawing.Size(43, 15)
+        Me.lblLogin.TabIndex = 0
+        Me.lblLogin.Text = "Login: "
+        '
+        'lblSenha
+        '
+        Me.lblSenha.AutoSize = True
+        Me.lblSenha.Location = New System.Drawing.Point(10, 72)
+        Me.lblSenha.Name = "lblSenha"
+        Me.lblSenha.Size = New System.Drawing.Size(45, 15)
+        Me.lblSenha.TabIndex = 1
+        Me.lblSenha.Text = "Senha: "
+        '
+        'txtLogin
+        '
+        Me.txtLogin.Location = New System.Drawing.Point(53, 16)
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(203, 23)
+        Me.txtLogin.TabIndex = 2
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Location = New System.Drawing.Point(53, 69)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtSenha.Size = New System.Drawing.Size(203, 23)
+        Me.txtSenha.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(53, 119)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnFechar
+        '
+        Me.btnFechar.Location = New System.Drawing.Point(143, 119)
+        Me.btnFechar.Name = "btnFechar"
+        Me.btnFechar.Size = New System.Drawing.Size(75, 23)
+        Me.btnFechar.TabIndex = 5
+        Me.btnFechar.Text = "Fechar"
+        Me.btnFechar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(268, 165)
+        Me.Controls.Add(Me.btnFechar)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.lblSenha)
+        Me.Controls.Add(Me.lblLogin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lanchonete do Xande"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblLogin As Label
+    Friend WithEvents lblSenha As Label
+    Friend WithEvents txtLogin As TextBox
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnFechar As Button
 End Class
