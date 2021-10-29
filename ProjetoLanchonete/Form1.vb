c0450f61ebc43192298af663afe4d5e030a1c0d3
Henrique Ernandes Rebelo <61748405+henriquernandes@users.noreply.github.com>
2021-10-29 18:10:58 -0300

    Projeto pronto para iniciar desenvolvimento

--- ProjetoLanchonete/Form1.vb
+++ ProjetoLanchonete/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
